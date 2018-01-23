a24b7bd9b62b5ea367c4244b92088323caaed5e1
phantomhieve <khetanatulz@gmail.com>
2018-01-23 18:45:08 +0530

    dynamic programming practice

---
+++ Dynamic Programming/coin_change.py
@@ -0,0 +1,64 @@
+## coin change problem
+##solution first
+dp={}
+def count(coins,money,size):
+    if (money,size) in dp:
+        return dp[(money,size)]
+    if money==0:
+        return 1
+    if money<0:
+        return 0
+    if size<=0 and money>=1:
+        return 0
+    ans=count(coins,money,size-1)+count(coins,money-coins[size-1],size)
+    dp[(money,size)]=ans
+    return ans
+
+
+
+##solution second
+def count_bottom(coins,money):
+    table = list()
+    
+    for i in xrange(money+1):
+        table.append([0]*len(coins))
+
+    for i in xrange(len(coins)):
+        table[0][i]=1
+        
+    for i in xrange(1,money+1):
+        for j in xrange(len(coins)):
+            x,y=0,0
+            ## including that coin (money-coins[size-1]),size-1
+            if i-coins[j]>=0:
+                x=table[i-coins[j]][j]
+            ##excuding that coin (money,size-1)
+            if j>=1:
+                y=table[i][j-1]
+            table[i][j]=x+y
+    return table[-1][-1]
+
+            
+##solution third
+def count_best(coins,money):
+    table = [0]*(money+1)
+    table[0]=1
+    for i in xrange(len(coins)):
+        for j in xrange(coins[i],money+1):
+            table[j]+=table[j-coins[i]]
+    return table[-1]
+
+money=500
+coins = [1,2,5,10,20,50,100]
+print count(coins,money,len(coins))
+print count_bottom(coins,money)
+print count_best(coins,money)
+
+
+
+
+
+
+
+        
+    
---
+++ Dynamic Programming/knapsack.py
@@ -0,0 +1,40 @@
+##knapsack problem
+
+##solution First
+dp={}
+def knap(capacity,length):
+    if (capacity,length) in dp:
+        return dp[(capacity,length)]
+    if capacity==0 or length==0:
+        return 0
+    if weight[length-1] > capacity:
+        ans=knap(capacity,length-1)
+        dp[(capacity,length)]=ans
+        return ans
+    else:
+        ans=max(value[length-1]+knap(capacity-weight[length-1],length-1),
+                   knap(capacity,length-1))
+        dp[(capacity,length)]=ans
+        return ans
+
+##solution second
+def knap_bottom(capacity,length):
+    table = list()
+    for i in xrange(capacity+1):
+        table.append([0]*(length+1))
+
+    for i in xrange(1,capacity+1):
+        for j in xrange(1,length+1):
+            if weight[j-1] <= i:
+                table[i][j]=max(value[j-1]+table[i-weight[j-1]][j-1],table[i][j-1])
+            else:
+                table[i][j]=table[i][j-1]
+    return table[-1][-1]
+
+value=[60,100,120]
+weight=[10,20,30]
+capacity=50
+print knap_bottom(capacity,len(value))
+print knap(capacity,len(value))
+
+
